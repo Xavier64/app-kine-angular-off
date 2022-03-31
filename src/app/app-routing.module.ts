05fb080cda4873f6a99264056be8fe4482aaec08
Pincon <xavierpincon@gmail.com>
2022-03-31 16:17:06 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DonComponent } from './pages/don/don.component';
+import { ExerciceComponent } from './pages/exercice/exercice.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotificationComponent } from './pages/notification/notification.component';
+import { ParametreComponent } from './pages/parametre/parametre.component';
+import { ProfilComponent } from './pages/profil/profil.component';
+import { ProgrammeComponent } from './pages/programme/programme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"programme",component:ProgrammeComponent},
+  {path:"exercice", component:ExerciceComponent},
+  {path:"profil", component:ProfilComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"parametre", component:ParametreComponent},
+  {path:"login", component:LoginComponent},
+  {path:"don", component:DonComponent},
+  {path:"inscription", component:InscriptionComponent},
+  {path:"notification", component:NotificationComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProgrammeComponent } from './pages/programme/programme.component';
+import { ExerciceComponent } from './pages/exercice/exercice.component';
+import { ProfilComponent } from './pages/profil/profil.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ParametreComponent } from './pages/parametre/parametre.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DonComponent } from './pages/don/don.component';
+import { NotificationComponent } from './pages/notification/notification.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProgrammeComponent,
+    ExerciceComponent,
+    ProfilComponent,
+    ContactComponent,
+    ParametreComponent,
+    LoginComponent,
+    DonComponent,
+    NotificationComponent,
+    InscriptionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
